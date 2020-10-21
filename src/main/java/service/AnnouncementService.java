5e513aec32b2cc774e8ac373aa3119af691dbd7d
kenraalikenobi <ville.lensu@gmail.com>
2020-10-21 17:06:12 +0300

    announcementservice skeleton

---
+++ src/main/java/service/AnnouncementService.java
@@ -0,0 +1,28 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.appengine.api.utils.SystemProperty;
+
+import conn.Connections;
+import data.Announcement;
+
+@Path("/announcementservice")
+public class AnnouncementService {
+
+}
