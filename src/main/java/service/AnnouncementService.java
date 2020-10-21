b737b7af42999053a9bb62910d37222f7c2870c3
kenraalikenobi <ville.lensu@gmail.com>
2020-10-21 18:19:34 +0300

    announcementservice fetchall

--- src/main/java/service/AnnouncementService.java
+++ src/main/java/service/AnnouncementService.java
@@ -24,5 +24,49 @@ import data.Announcement;
 
 @Path("/announcementservice")
 public class AnnouncementService {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/getallannouncements")
+	public ArrayList<Announcement> getAllAnnouncements() {
+		String sql = "select * from announcement";
+		ResultSet RS = null;
+		ArrayList<Announcement> list = new ArrayList<>();
+
+		Connection conn = null;
+		try {
+			if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+				conn = Connections.getProductionConnection();
+			} else {
+				conn = Connections.getDevConnection();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			RS = stmt.executeQuery(sql);
+			while (RS.next()) {
+				Announcement c = new Announcement();
+				c.setId(RS.getInt("id"));
+				c.setDate(RS.getString("date"));
+				c.setTitle(RS.getString("title"));
+				c.setContent(RS.getString("content"));
+				list.add(c);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
 
 }
